a9e03f94f153ee183a52b9b816939362d92dfd00
Abdullaev-kosimzho-002 <dzukosta348@gmail.com>
2022-02-27 10:36:33 +0300

    Добавьте файлы проекта.

---
+++ Example_2/Program.cs
@@ -0,0 +1,447 @@
+﻿using System;
+
+namespace Example_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if-Task
+
+            //Number 1.
+            /*
+            int number_1 = Convert.ToInt32(Console.ReadLine());
+            int number_2 = Convert.ToInt32(Console.ReadLine());
+
+            if((number_1 * number_1 + number_2 * number_2) > (number_1 + number_2) * (number_1 + number_2) || (number_1 * number_1 + number_2 * number_2) < (number_1 + number_2) * (number_1 + number_2))
+            {
+                Console.WriteLine("What is the biggest");
+            }
+            */
+
+            // Number 2
+            /*
+            int number_1 = Convert.ToInt32(Console.ReadLine());
+
+         if(number_1 > 1 || number_1 == 3)
+            {
+                Console.WriteLine("Четное");
+            }else
+            {
+                Console.WriteLine("Оканчивается 3");
+            }
+         */
+
+            //Number 3.
+            /*
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number_2 = Convert.ToInt32(Console.ReadLine());
+
+            if(number1 * number1 - number_2 * number_2 > number1 - number_2 || number1 - number_2 > number1 * number1 - number_2 * number_2)
+            {
+                Console.WriteLine("First");
+            }
+            else
+            {
+                Console.WriteLine("Second");
+            }
+            */
+
+            //Number 4
+            /*
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int number_1 = number / 2;
+            if(number_1 > 0 || number == 7)
+            {
+                Console.WriteLine("Четное");
+            }else
+            {
+                Console.WriteLine("Нечетное");
+            }
+            */
+
+            // Number 5.
+
+            /*      int months = Convert.ToInt32(Console.ReadLine());
+
+                  switch(months)
+                  {
+                      case 1:Console.WriteLine("Январь"); 
+                          break;
+
+                      case 2: Console.WriteLine("Февраль");
+                          break;
+                      case 3: Console.WriteLine("Март");
+                          break;
+                      case 4: Console.WriteLine("Апрель");
+                          break;
+                      case 5: Console.WriteLine("Май");
+                          break;
+                      case 6: Console.WriteLine("Июнь");
+                          break;
+                      case 7: Console.WriteLine("Июль");
+                          break;
+                      case 8: Console.WriteLine("Август");
+                          break;
+                      case 9: Console.WriteLine("Сентябрь");
+                          break;
+                      case 10: Console.WriteLine("Октябрь");
+                          break;
+                      case 11: Console.WriteLine("Ноябрь");
+                          break;
+                      case 12: Console.WriteLine("Декабрь");
+                          break;
+
+                      default: Console.WriteLine("Warning");
+                          break;
+
+                  }
+
+                  */
+
+
+
+            // Operation
+
+
+            //Number 1.
+            /*
+            int number_1 = 3;
+            double number_2 = 3.5;
+            double number_3 = -2.1;
+
+            Console.WriteLine(number_1 + number_2 + number_3 / 3);
+
+            */
+
+            //Number 2.
+
+            /*
+            int number_1 = 56;
+            int number_2 = 38;
+
+            Console.WriteLine(number_1 + number_2);
+            */
+
+
+            //Number 3.
+
+            /*
+            int number_1 = 46;
+            int number_2 = 78;
+
+            Console.WriteLine(number_1 * number_2);
+            */
+
+            //Number 4.
+            /*
+            int R1 = 5;
+            int R2 = 10;
+            int R3 = 2;
+            int fullR = R1 + R2 + R3;
+
+            Console.WriteLine(R1 + R2 + R3);
+            Console.WriteLine(fullR);
+            */
+
+
+            //Циклы
+
+            //Number 1.
+
+            /*
+            int number_1 = Convert.ToInt32(Console.ReadLine());
+            int number_2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            for(int i = number_1; i < number_2; i++)
+            {
+                int n = number_1 + number_2 + 1;
+                Console.WriteLine(n);
+            }
+            */
+
+
+            //Number 2.
+            /* 
+             int nNumber = 10;
+             int integerNumber = nNumber / 5;
+
+            for(int i = integerNumber; i > 1; i++)
+             {
+                 Console.WriteLine();
+             }
+
+             */
+
+            //Массивы 
+
+            //Number 1.
+            /*
+            int[] numbers = new int[14];
+            int nuber_1 = numbers.Length / 2;
+            Console.WriteLine(nuber_1);
+            */
+
+
+            //Number 2.
+            /*
+            int[] integer =  { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+            integer[5] = 10;
+            Console.WriteLine(integer[5]);
+            */
+
+            //Number 3.
+            /*
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(numbers.Length);
+            */
+
+            //Number 4.
+
+            /*
+            int[] maxValue = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            maxValue[0] = maxValue[9];
+            Console.WriteLine(maxValue[0]);
+            maxValue[9] = maxValue[0];
+            Console.WriteLine(maxValue[9]);
+            */
+
+            //Number 5.
+            /*
+            int[] integer_Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+       Console.WriteLine(integer_Numbers.Length);
+       int numerals   = integer_Numbers.Length + integer_Numbers.Length / 2;
+            Console.WriteLine(numerals);
+
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
